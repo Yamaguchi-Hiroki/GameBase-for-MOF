e48b4b9a9c3ea01c320f98327d3ebc298edc681a
Yamaguchi-Hiroki <91718299+Yamaguchi-Hiroki@users.noreply.github.com>
2021-10-29 11:05:01 +0900

    reset

---
+++ Project/TitleScene.cpp
@@ -0,0 +1,43 @@
+#include "TitleScene.h"
+#include <Mof.h>
+
+using namespace Game;
+
+// コンストラクタ
+TitleScene::TitleScene()
+{
+}
+
+// デストラクタ
+TitleScene::~TitleScene()
+{
+}
+
+// 読み込み
+bool TitleScene::Load()
+{
+    return true;
+}
+
+// 初期化
+void TitleScene::Initialize()
+{
+}
+
+// 更新
+void TitleScene::Update()
+{
+}
+
+// 描画
+void TitleScene::Render()
+{
+    CGraphicsUtilities::RenderString(
+        0, 0,"TITLE SCENE"
+    );
+}
+
+// 解放
+void TitleScene::Release()
+{
+}
